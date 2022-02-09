2727f09db5dcbdeeee59cc22acc2ac00b6b0f9f7
Michel Deghaud <michel.deghaud@epitech.eu>
2022-02-09 16:33:08 +0100

    creation des components

---
+++ Components/ListClub.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+class ListClub extends React.Component {
+    render() {
+        return (
+           <View></View> 
+        )
+    }
+}
+
+export default ListClub
